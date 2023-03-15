0ecd2979c0dfb7ef69e6a1432685deb202a15d8f
SparrowSSSS <vlad22832006@gmail.com>
2023-03-15 13:56:02 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 window.onload = () => {
-    
+    const burgerMenuBTN = document.querySelector(".burger-menu");
+    const headerMenu = document.querySelector(".menu");
+
+    burgerMenuBTN.addEventListener("click", () => {
+        let activeBurgerIcon = document.querySelector(".burger-menu__icon[data-active='true']");
+        let notActiveBurgerIcon = document.querySelector(".burger-menu__icon[data-active='false']");
+
+        activeBurgerIcon.style.display = "none";
+        notActiveBurgerIcon.style.display = "block";
+
+        activeBurgerIcon.dataset.active = "false";
+        notActiveBurgerIcon.dataset.active = "true";
+
+        if (burgerMenuBTN.dataset.active == "false") {
+            headerMenu.style.display = "flex";
+            burgerMenuBTN.dataset.active = "true";
+        } else {
+            headerMenu.style.display = "none";
+            burgerMenuBTN.dataset.active = "false";
+        };
+    });
 };
